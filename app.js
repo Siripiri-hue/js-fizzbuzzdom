4276f7aa1ee844186c415723109b2cccc806dbb6
daemonprocellae@live.it <daemonprocellae@live.it>
2022-02-07 15:11:55 +0100

    Aggiunta lista a html

--- app.js
+++ app.js
@@ -3,17 +3,33 @@ title.innerHTML = `FizzBuzz DOM`;
 document.body.appendChild(title);
 title.classList.add("centered");
 
-
-
+const list = document.createElement("ul");
+document.body.appendChild(list);
 
 for (let i=0; i < 100; i++)
 {
+    const listItem = document.createElement("li");
+    let content = "";
     if ( ( (i+1) % 3 === 0) && ( (i+1) % 5 === 0) )
-        console.log(`BuzzFuzz`);
+    {
+        // console.log(`FizzBuzz`);
+        listItem.innerHTML = `FizzBuzz`;
+    } 
     else if ( (i+1) % 3 === 0)
-        console.log(`Buzz`);
+    {
+        // console.log(`Fizz`);
+        listItem.innerHTML = `Fizz`;
+    }
     else if ( (i+1) % 5 === 0)
-        console.log(`Fuzz`);
+    {
+        // console.log(`Buzz`);
+        listItem.innerHTML = `Buzz`;
+    }
     else
-        console.log(i+1);
+    {
+        // console.log(i+1);
+        listItem.innerHTML = `${i+1}`;
+    }
+    list.append(listItem);
+        
 }
